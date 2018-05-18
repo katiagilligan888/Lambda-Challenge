1c974857c2304f351550cdd5042ad4fcd3c11c4a
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-05-18 16:40:04 -0600

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+    return "Hello World!";
 }
 
 /*
@@ -23,8 +23,16 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if((num % 3 === 0) && (num % 5 === 0)){
+    return "Lambda School";
+  }else if(num % 3 === 0){
+    return "Lambda";
+  }else if(num % 5 === 0) {
+    return "School";
+  }else{
+    return num;
+  }
 }
 
 /*
@@ -38,8 +46,16 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
+function longestString(strs) {
+  let largestStringLength = 0;
+  let largestString = ''
+  for( let x= 0; x < strs.length;  x++){
+    if (strs[x].length > largestStringLength){
+      largestStringLength = strs[x].length
+      largestString = strs[x];
+    }
+  }
+  return largestString; 
 }
 
 /*
@@ -63,8 +79,18 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
-
+function computeUserAverageAge(users) {
+  let numArr = [];
+  let numberTotal = 0
+    for(let x = 0; x < users.length; x++){
+      let age = users[x].age
+      numArr.push(age);
+    }
+    for(let i = 0; i < numArr.length; i++){
+      numberTotal += numArr[i];
+    }
+    let avg = Math.ceil(numberTotal/ (numArr.length));
+    return avg;
 }
 
 module.exports = {
